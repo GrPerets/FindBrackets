276050e1f2937f201730ad109556b0d109dc6cd1
GrPerets <grperets@gmail.com>
2020-07-25 14:54:21 +0300

    Add BigInteger

--- src/com/grperets/main/Main.java
+++ src/com/grperets/main/Main.java
@@ -1,5 +1,6 @@
 package com.grperets.main;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -9,8 +10,8 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of pairs of brackets:  ");
         int n = 0;
-        if ((scanner.hasNextInt())) {
-            n = scanner.nextInt();
+        if ((scanner.hasNextInt())&&(n = scanner.nextInt())>0?true:false) {
+
         } else throw new NumberFormatException();
 
 
@@ -23,21 +24,27 @@ public class Main {
          *
          *
          *
-         *   This code finds the maximum number of nodes
+         *   This code finds the maximum number of paths
          */
 
-        int[][] arrayWays = new int [n][];
-        arrayWays[0] = new int[n];
+        BigInteger[][] arrayWays = new BigInteger [n][];
+        arrayWays[0] = new BigInteger[n];
 
         for ( int i=0; i<arrayWays[0].length; i++ ){
-            arrayWays[0][i] = 1;
+            arrayWays[0][i] = BigInteger.valueOf(1);
+
         }
 
         for (int i=1; i<n; i++){
-            arrayWays[i] = new int[n-i];
+            arrayWays[i] = new BigInteger[n-i];
+
             for(int j=0; j<n-i; j++){
+                arrayWays[i][j]= BigInteger.valueOf(0);
                 for(int k=0; k<j+2; k++){
-                    arrayWays[i][j] += arrayWays[i-1][k];
+                    arrayWays[i][j] = arrayWays[i][j].add(arrayWays[i-1][k]);
+
+                    //An old method with int or long
+                    //arrayWays[i][j] += arrayWays[i-1][k];
                 }
             }
         }
